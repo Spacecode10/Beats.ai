d9c24adbe9a9c334268226250414b13a751e4f7b
Nakshatra Vyas <nakshatravyas2004@gmail.com>
2024-02-23 23:14:10 +0530

    Created genre to music page

--- frontend/src/pages/Genre.js
+++ frontend/src/pages/Genre.js
@@ -2,10 +2,78 @@ import React from 'react'
 
 export default function Genre() {
   return (
-    <section>
-        <div className='container'>
-            hi
+    <section className='genre'>
+      <div className='container'>
+        <h1>Select genres</h1>
+        <div className="genre-div">
+          <div>
+            <label>
+              <input type="checkbox" name=''/>
+              <span>Pop</span>
+            </label>
+          </div>
+          <div>
+            <label>
+              <input type="checkbox" name=''/>
+              <span>Rock</span>
+            </label>
+          </div>
+          <div>
+            <label>
+              <input type="checkbox" name=''/>
+              <span>Jazz</span>
+            </label>
+          </div>
+          <div>
+            <label>
+              <input type="checkbox" name=''/>
+              <span>Electronic</span>
+            </label>
+          </div>
+          <div>
+            <label>
+              <input type="checkbox" name=''/>
+              <span>Classical</span>
+            </label>
+          </div>
+          <div>
+            <label>
+              <input type="checkbox" name=''/>
+              <span>Metal</span>
+            </label>
+          </div>
+          <div>
+            <label>
+              <input type="checkbox" name=''/>
+              <span>Funk</span>
+            </label>
+          </div>
+          <div>
+            <label>
+              <input type="checkbox" name=''/>
+              <span>House</span>
+            </label>
+          </div>
+          <div>
+            <label>
+              <input type="checkbox" name=''/>
+              <span>Trap</span>
+            </label>
+          </div>
+          <div>
+            <label>
+              <input type="checkbox" name=''/>
+              <span>Hip Hop</span>
+            </label>
+          </div>
+          <div>
+            <label>
+              <input type="checkbox" name=''/>
+              <span>Hip Hop</span>
+            </label>
+          </div>
         </div>
+      </div>
     </section>
   )
 }
--- frontend/src/pages/Prompt.js
+++ frontend/src/pages/Prompt.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Prompt() {
+  const [time, setTime] = useState(10);
   return (
     <section className='prompt'>
       <div className='shape1'></div>
@@ -10,13 +11,13 @@ export default function Prompt() {
         <div className="prompt-div">
           <h1>Generate music from text</h1>
           <form className='prompt-form'>
-            <input type="text" className='prompt-input' placeholder='A retro melody with piano and sitar' />
+            <input type="text" className='prompt-input' placeholder='"A retro melody with piano and sitar"' />
             <div className='duration-div'>
               <h3>Duration</h3>
-              <span id="slider-value">8 S</span>
+              <span id="slider-value">{time} Sec</span>
             </div>
             <div className='slider-div'>
-              <input type="range" min="1" max="30" value="8" />
+              <input type="range" min="1" max="30" value={time} onChange={(e) => setTime(e.target.value)} />
             </div>
             <button className='btn' type='submit'>Generate</button>
           </form>
